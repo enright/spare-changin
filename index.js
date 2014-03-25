dcdf5468f94c40f7dc0b2f3067edf159660b0ef7
Bill Enright <bill@yisup.com>
2014-03-24 23:39:59 -0700

    dust helpers, formatting

--- index.js
+++ index.js
@@ -4,6 +4,9 @@
 var kraken = require('kraken-js'),
     app = {};
 
+//Load our custom helper
+require('./lib/helper-formatDate');
+require('./lib/helper-formatCurrencyAmount');
 
 app.configure = function configure(nconf, next) {
     // Async method run on startup.
--- models/activity.js
+++ models/activity.js
@@ -9,18 +9,21 @@ module.exports = function ActivityModel() {
 			type: 'Purchase',
 			place: 'Macys',
 			currencyCode: 'USD',
+			symbol: '$',
 			amount: 99.99
 			},
 			id2: {date: new Date(2014, 1, 2),
 			type: 'Purchase',
 			place: 'Home Depot',
 			currencyCode: 'USD',
+			symbol: '$',
 			amount: 150.15
 			},
 			id3: {date: new Date(2013, 12, 31),
 			type: 'Transfer',
 			place: 'Wells Fargo',
 			currencyCode: 'USD',
+			symbol: '$',
 			amount: 350.00
 			}
 		}
