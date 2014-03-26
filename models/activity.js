e20e2b04a38f64567a2c4ec3e59e8d4c3e0527bb
Bill Enright <bill@yisup.com>
2014-03-25 22:59:21 -0700

    alternate row coloring, added transactions

--- models/activity.js
+++ models/activity.js
@@ -25,6 +25,41 @@ module.exports = function ActivityModel() {
 			currencyCode: 'USD',
 			symbol: '$',
 			amount: 350.00
+			},
+			id4: {date: new Date(2013, 12, 15),
+			type: 'Purchase',
+			place: 'Jamba Juice',
+			currencyCode: 'USD',
+			symbol: '$',
+			amount: 2.30
+			},
+			id5: {date: new Date(2013, 11, 1),
+			type: 'Refund',
+			place: 'DSW Shoes',
+			currencyCode: 'USD',
+			symbol: '$',
+			amount: 75.45
+			},
+			id6: {date: new Date(2013, 10, 20),
+			type: 'Purchase',
+			place: 'DSW Shoes',
+			currencyCode: 'USD',
+			symbol: '$',
+			amount: 75.45
+			},
+			id7: {date: new Date(2013, 12, 31),
+			type: 'Purchase',
+			place: 'Jamba Juice',
+			currencyCode: 'USD',
+			symbol: '$',
+			amount: 3.00
+			},
+			id8: {date: new Date(2013, 12, 31),
+			type: 'Transfer',
+			place: 'Wells Fargo',
+			currencyCode: 'USD',
+			symbol: '$',
+			amount: 2.75
 			}
 		}
 	};
